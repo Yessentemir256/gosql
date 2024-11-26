e0d5edc17fb9114ab7d4d69c775c243adbb1d2c8
Yessentemir <yessentemir.utepov@gmail.com>
2024-11-26 19:56:34 +0500

    connected with database and created the new table

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"log"
 	"os"
-
-	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
 	// адрес подключения
 	// протокол://логин:пароль@хост:порт/бд
-	dsn := "postgres://app:pass@localhost:5432/db"
+	dsn := "postgres://app:pass@localhost:5434/db"
 	// получение указателя на структуру для работы с БД
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
@@ -25,5 +25,18 @@ func main() {
 			log.Println(err)
 		}
 	}()
-	// TODO: запросы
+	ctx := context.Background()
+	_, err = db.ExecContext(ctx, `
+	  CREATE TABLE IF NOT EXISTS customers (
+		id      BIGSERIAL PRIMARY KEY,
+		name    TEXT  NOT NULL,
+		phone   TEXT NOT NULL UNIQUE,
+		active  BOOLEAN NOT NULL DEFAULT TRUE,
+		created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	)
+	`)
+	if err != nil {
+		log.Print(err)
+		return
+	}
 }
